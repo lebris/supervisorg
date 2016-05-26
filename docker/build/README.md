1e5868ecba77f8321421d52f73710d32be6671ab
Claude LE BRIS <claude.lebris@gmail.com>
2016-05-26 16:31:36 +0200

    feat: add helper to build supervisorg images for publication on docker hub

--- docker/build/README.md
+++ docker/build/README.md
@@ -1,14 +1,31 @@
-# Build a supervisorg docker image
-## Build the image
+# Build supervisorg docker images
+## Build the images
 
 ```shell
-$ ./build.sh <git ref>
+$ ./build.sh <git ref> <tag>
 ```
+
+This command will build the supervisorg and workers image.
 
 Parameters :
  * `<git_ref>` : a git commit hash, tag or branch name
+ * `<tag>` : tag to assign to the built images
 
 This will clone the sources, prepare the application (dependencies, assets, etc.), and copy (to `/var/www/supervisorg`) supervisorg sources in the image.
+
+Example:
+```
+$ ./build.sh master latest
+```
+
+## Prerequisites
+
+Before running this command you must build the supervisorg/packager image.
+
+```
+$ cd path/to/supervisorg/docker/packager
+$ make
+```
 
 ## Test the built image
 
--- docker/build/build.sh
+++ docker/build/build.sh
@@ -3,24 +3,69 @@
 set -e
 set -u
 
-[ $# -ne 1 ] && { echo "Usage: $0 git reference (hash, tag, branch name)"; exit 1; }
+[ $# -ne 2 ] && { echo "Usage: $0 [git reference] [tag]"; exit 1; }
 
 GIT_REF=$1
+TAG=$2
+
+SRC_DIR=src
+APP_SRC_DIR=/var/www/supervisorg
+
+cleanup() {
+    for DIR in src supervisorg/src workers/src; do
+        if [ -d "$(pwd)/${DIR}" ]; then
+            echo "Cleaning directory ${DIR}"
+            docker run --rm \
+                       -v $(pwd)/${DIR}:/tmp/${DIR} \
+                       busybox \
+                       sh -c "rm -rf /tmp/${DIR}/*"
+
+           rm -rf ${DIR}
+        fi
+    done
+}
 
 ORIGINAL_DIR=$(pwd)
-[ -d src ] && { echo "The src directory already exists." ; exit 1; }
-git clone https://github.com/lebris/supervisorg.git src
-cd src
+
+cleanup
+
+git clone https://github.com/lebris/supervisorg.git ${SRC_DIR}
+OLD_PWD=$(pwd)
+cd ${SRC_DIR}
 git checkout $GIT_REF
+cd ${OLD_PWD}
+unset OLD_PWD
 
-mkdir -p var www/css www/js www/fonts
-php composer.phar install --no-dev --optimize-autoloader
-sh ./lessc
-vendor/bin/karma hydrate --env=prod
-php console assetic:dump
+docker run -v $(pwd)/${SRC_DIR}:${APP_SRC_DIR} \
+           -v ~/.composer:/root/.composer \
+           --rm \
+           --name=supervisorg_packager \
+           -ti \
+           supervisorg/packager \
+           /bin/su -c 'cd /var/www/supervisorg && \
+                       php composer.phar install --no-dev --optimize-autoloader && \
+                       vendor/bin/karma hydrate --env=prod && \
+                       sh ./lessc && \
+                       php package assetic:dump && \
+                       chown -Rf www-data:www-data /var/www/supervisorg/*'
 
-cd $ORIGINAL_DIR
+SERVICE_LIST='supervisorg workers'
+for SERVICE in $SERVICE_LIST;do
+    OLD_PWD=$(pwd)
 
-make
+    cd ${SERVICE}
 
-rm -rf src
+    mv ../src .
+    # .dockerignore file must be at the root directory of the context
+    cp src/.dockerignore .
+
+    make TAG=${TAG}
+
+    rm .dockerignore
+    mv src ../
+
+    cd ${OLD_PWD}
+    unset OLD_PWD
+done
+
+cleanup
